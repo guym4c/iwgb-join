a19f1729daad4317e03cb2504da0d8801479a4f9
Guy Mac <hello@guymac.eu>
2019-07-03 17:10:32 +0100

    Add GC webhook processing

--- settings.php
+++ settings.php
@@ -5,6 +5,9 @@
 $keys = require_once APP_ROOT . '/keys.php';
 
 return [
+    'gocardless' => [
+        'webhookSecret' => $keys['gocardless']['webhook'],
+    ],
     'sentry' => [
         'dsn' => $keys['sentry'],
     ],
---
+++ src/Action/Hook/GoCardlessAction.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace IWGB\Join\Hook;
+
+use IWGB\Join\Action\GenericAction;
+use Psr\Http\Message\ResponseInterface;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use GoCardlessPro as GoCardless;
+
+class GoCardlessAction extends GenericAction {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __invoke(Request $request, Response $response, array $args): ResponseInterface {
+
+        try {
+            $events = GoCardless\Webhook::parse($request->getParsedBody(),
+                $request->getHeader('Webhook-Signature'),
+                $this->settings['gocardless']['webhookSecret']);
+
+        } catch (GoCardless\Core\Exception\InvalidSignatureException $e) {
+            $this->log->addError($e->getMessage());
+            return $response->withStatus(498);
+        }
+
+        foreach ($events as $event) {
+            // do something
+        }
+
+        return $response->withStatus(204);
+    }
+}
