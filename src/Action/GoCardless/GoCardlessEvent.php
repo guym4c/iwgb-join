d3566a4faaa16c3f81cb765ba36e86ad158147ec
Guy Mac <hello@guymac.eu>
2019-08-07 20:06:44 +0100

    Update member payment status processing

--- src/Action/GoCardless/GoCardlessEvent.php
+++ src/Action/GoCardless/GoCardlessEvent.php
@@ -1,10 +1,12 @@
 <?php
+/** @noinspection PhpUndefinedFieldInspection */
 
 namespace IWGB\Join\Action\GoCardless;
 
 use GoCardlessPro as GoCardless;
 use Guym4c\Airtable\AirtableApiException;
 use Guym4c\Airtable\ListFilter;
+use Guym4c\Airtable\Record;
 use Psr\Http\Message\ResponseInterface;
 use Slim\Http\Request;
 use Slim\Http\Response;
@@ -30,28 +32,39 @@ class GoCardlessEvent extends GenericGoCardlessAction {
 
         foreach ($events as $event) {
 
+
             /** @var $event GoCardless\Resources\Event */
             switch ($event->action) {
                 case 'failed':
-
-                    $payment = $this->gocardless->payments()->get($event->links->payment);
-                    $mandate = $this->gocardless->mandates()->get($payment->links->mandate);
-
-                    $member = $this->airtable->list('Members', (new ListFilter())
-                        ->setFormula("SEARCH('{$mandate->links->customer}', {Customer ID})"))
-                                  ->getRecords()[0];
 
                     $this->airtable->create('Missed Payments', [
                         'GoCardless ID' => $event->id,
                         'Reason'        => $event->details->cause,
                         'Reason detail' => $event->details->description,
-                        'Member'        => [$member->getId()],
+                        'Member'        => [$this->getMemberFromPayment($this->gocardless->payments()
+                            ->get($event->links->payment))->getId()],
                     ]);
-
                     break;
+
+                case 'cancelled':
+                    $member = $this->getMemberFromPayment($this->gocardless->payments()
+                        ->get($event->links->payment));
+                    $member->Status = 'Cancelled';
+                    $this->airtable->update($member);
             }
         }
 
         return $response->withStatus(StatusCode::HTTP_NO_CONTENT);
+    }
+
+    /**
+     * @param GoCardless\Resources\Payment $payment
+     * @return Record
+     * @throws AirtableApiException
+     */
+    private function getMemberFromPayment(GoCardless\Resources\Payment $payment): Record {
+        return $this->airtable->list('Members', (new ListFilter())
+            ->setFormula("SEARCH('{$this->gocardless->mandates()->get($payment->links->mandate)->links->customer}', {Customer ID})"))
+                      ->getRecords()[0];
     }
 }
